a789a8ce002274563d5b99e8ec641bb0bf995976
Kairo Chácara <144383548+ChacaraKairo@users.noreply.github.com>
2026-07-31 20:25:56 -0300

    test: add application context test

---
+++ api/src/test/java/br/com/chacarakairo/validatordoc/ValidatorDocApplicationTests.java
@@ -0,0 +1,18 @@
+package br.com.chacarakairo.validatordoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:validator_doc;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.flyway.enabled=false",
+        "spring.jpa.hibernate.ddl-auto=create-drop"
+})
+class ValidatorDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
